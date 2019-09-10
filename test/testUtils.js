785b42b6611e25d2af40dce2f58813251265e087
Erkan Ertürk <erkanerturk096@gmail.com>
2019-09-10 17:14:46 +0300

    Redux Testing -> Connect & Action & Reducer

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,22 @@
 import checkPropTypes from 'check-prop-types';
 import { ShallowWrapper } from 'enzyme';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ *  globals: rootReducer, middlewares.
+ * @param {object} initialState - Initial state for store.
+ * @function storeFactory
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState);
+  // const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  // return createStoreWithMiddleware(rootReducer, initialState);
+};
 
 /**
  * Return ShallowWrapper containing node(s) with the given data-test value.
